3c4946b679b3abb20d8af774406990154394cd34
zcer <1391656358@qq.com>
2020-01-08 15:09:56 +0800

    add

---
+++ 0001-Two-Sum/php/solution1.php
@@ -0,0 +1,31 @@
+<?php
+/// Source : https://leetcode.com/problems/two-sum/description/
+/// Author : zcer
+/// Time   : 2019-01-08
+
+class Solution
+{
+    /**
+     * @param Integer[] $nums
+     * @param Integer   $target
+     * @return Integer[]
+     *
+     *  Brute Force.
+     *  Time Complexity: O(n^2)
+     *  Space Complexity: O(1)
+     */
+    function twoSum($nums, $target)
+    {
+        $ret = [];
+        for ($i = 0; $i < count($nums); $i++) {
+            for ($j = $i + 1; $j < count($nums); $j++) {
+                if ($nums[$i] + $nums[$j] == $target) {
+                    $ret[] = $i;
+                    $ret[] = $j;
+                    return $ret;
+                }
+            }
+        }
+        return $ret;
+    }
+}
