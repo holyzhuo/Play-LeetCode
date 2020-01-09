b59c314d72148620ccec5f3d3f1bdb308f26745f
zcer <1391656358@qq.com>
2020-01-09 15:47:01 +0800

    0053 solved

---
+++ 0053-Maximum-Subarray/php/solution1.php
@@ -0,0 +1,34 @@
+<?php
+/// Source : https://leetcode.com/problems/maximum-subarray/
+/// Author : zcer
+/// Time   : 2019-01-09
+
+class Solution
+{
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer
+     *
+     * 从头到尾遍历每一个数组元素，如何前面元素的和为正，则加上本元素的值继续搜索；如何前面元素的和为负，则此元素开始新的和计数。整个过程中要注意更新和的最大值
+     * Time Complexity: O(n)
+     * Space Complexity: O(1)
+     */
+    function maxSubArray($nums)
+    {
+        $max = $nums[0];
+        $sum = $max;
+        for ($i = 1; $i < count($nums); $i++) {
+            if ($sum + $nums[$i] > $nums[$i]) {
+                $sum += $nums[$i];
+            } else {
+                $sum = $nums[$i];
+            }
+
+            if ($max < $sum) {
+                $max = $sum;
+            }
+        }
+        return $max;
+    }
+}
