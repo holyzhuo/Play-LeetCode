285cf2c22f8c46f5ac5f50c0cee7820ae6e92a4d
zcer <1391656358@qq.com>
2020-01-14 18:22:37 +0800

    change

---
+++ 0283-Move-Zeroes/php/solution1.php
@@ -0,0 +1,15 @@
+<?php
+/// Source : https://leetcode.com/problems/move-zeroes/
+/// Author : zcer
+/// Time   : 2019-01-12
+
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return NULL
+     */
+    function moveZeroes(&$nums) {
+        
+    }
+}
